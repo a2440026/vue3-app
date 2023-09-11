b73505d3442b177d01114050a67f0e45ed1039b0
songjia <songjia@wakedata.com>
2023-09-11 18:07:31 +0800

    feat: '去掉多余文件'

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-export default {
-  proxy: {
-    "/api": {
-      target: "http://",
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
