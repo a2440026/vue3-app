ed9f6768d5b2340f5bf1a6a3c493c22b57fdfd15
songjia <1104667585@qq.com>
2023-09-12 22:34:32 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, normalizePath } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+
+const variablePath = normalizePath(path.resolve('./src/styles/global.less'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `@import "${variablePath}";`
+      }
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['> 1%', 'last 2 versions']
+        })
+      ]
+    }
+  }
+});
