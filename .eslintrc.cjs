afba34233c810d89543cc8b2e159c546a00015ef
songjia <1104667585@qq.com>
2023-09-12 23:08:22 +0800

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': 0,
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'eqeqeq': ['error', 'always']
